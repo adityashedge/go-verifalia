56097e9ea509ea06273f627212953ebf43222a55
Aditya Shedge <aditya@joshsoftware.com>
2015-12-13 17:21:48 +0530

    Close #1 added 'NewRequest' method for client to generate API requests
    - returns URL object which should be used to make API requests.
    - all API requests will be made using this generic method.

--- verifalia/verifalia.go
+++ verifalia/verifalia.go
@@ -1,12 +1,19 @@
 // Package verifalia provides an API client for Verifalia API. For more details, see http://verifalia.com/developers
 package verifalia
 
-import "net/url"
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+)
 
 const (
 	libraryVersion = "0.1"
 	defaultBaseUrl = "https://api.verifalia.com/v1.1/"
 	userAgent      = "go-verifalia/" + libraryVersion
+	contentType    = "application/json"
 )
 
 // A Client manages communication with the Verifalia REST API.
@@ -39,4 +46,40 @@ func NewClient(accountSID, authToken string) *Client {
 		BaseURL:    baseUrl,
 	}
 	return c
+}
+
+// NewRequest creates an API request.
+// method is the HTTP VERB
+// path is the relative URL resolved relative to the BaseURL of the Client (eg. "email-validations").
+// Relative URLs should always be specified without a preceding slash.
+// It can also be an absolute URL.
+// If specified, the value pointed to by body is JSON encoded and included as the request body.
+func (c *Client) NewRequest(method, path string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+	url := c.BaseURL.ResolveReference(rel)
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err = json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, url.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	// Verifalia uses basic auth
+	req.SetBasicAuth(c.AccountSID, c.AuthToken)
+	req.Header.Add("Content-Type", contentType)
+	if c.UserAgent != "" {
+		req.Header.Add("User-Agent", c.UserAgent)
+	}
+	return req, nil
 }
