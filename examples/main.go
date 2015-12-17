2ac8c79fc78f50de9e155c715f0a5f2d22408b8b
Aditya Shedge <aditya@joshsoftware.com>
2015-12-18 00:37:32 +0530

    Ref #4 Updated example for API to get the status of an email validation job

--- examples/main.go
+++ examples/main.go
@@ -9,12 +9,22 @@ import (
 func main() {
 	// add a valid account subscription ID and auth token(or sub-account password)
 	c := verifalia.NewClient("Account SID", "Auth Token")
+
 	emails := []string{"john.smith@example.com", "foo@example.net"}
+
+	fmt.Println("-------------> Validate")
 	resp, err := c.Validate(emails)
 	if err != nil {
 		panic(err)
-	} else {
-		fmt.Printf("Response:\t%+v\n", resp)
-		fmt.Printf("Server response:\t%+v\n", resp.Data)
 	}
+	fmt.Printf("Response:\t%+v\n", resp)
+	fmt.Printf("Server response:\t%+v\n", resp.Data)
+
+	fmt.Println("-------------> Query")
+	resp, err := c.Query(resp.UniqueID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Response:\t%+v\n", resp)
+	fmt.Printf("Server response:\t%+v\n", resp.Data)
 }
