fa57159f19b1db445864677f009243de1909c537
Aditya Shedge <aditya@joshsoftware.com>
2015-12-19 01:52:51 +0530

    Ref #7 Added API for deleting an existing email validation job
    - If the job is completed and deleted, it returns 200 (Ok)
    - If the job is not completed it returns 406 (Not acceptable)
    - If the job does not exist then it returns 404(Not found)

--- verifalia/verifalia.go
+++ verifalia/verifalia.go
@@ -158,6 +158,40 @@ func (c *Client) Query(uniqueID string) (*Response, error) {
 	return buildResponse(resp)
 }
 
+// Query the Verifalia API to delete a specific validation job.
+// In order to use this API, you need to pass a unique job ID as a string argument.
+// The email validation job must be completed on the server or else server will return 406
+// DELETE: https://api.verifalia.com/v1.1/email-validations/{uniqueID}
+func (c *Client) Delete(uniqueID string) (*Response, error) {
+	if uniqueID == "" {
+		err := errors.New("unique job ID should not be an empty string")
+		log.Fatalln(err)
+		return nil, err
+	}
+	// create the request URL using uniqueID
+	url := fmt.Sprintf("email-validations/%v", uniqueID)
+	// build request object for email validation job status API
+	req, err := c.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+	log.Println(req.URL)
+	// send request to the API
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+	// close request body after "Query" method executes
+	defer resp.Body.Close()
+	r := Response{
+		StatusCode: resp.StatusCode,
+		Status:     http.StatusText(resp.StatusCode),
+	}
+	return &r, nil
+}
+
 // Since server response is same for 'Validate' and 'Query',
 // extract it out in a separate method which will be used by both methods.
 // This is a private method not exported by the package.
